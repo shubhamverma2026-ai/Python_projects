fc147e2ecbc74628050216299883eb171483086b
shubhamverma2026-ai <shubham.verma2026@iic.ac.in>
2026-08-30 08:47:48 +0530

    Update music.py

--- music/music.py
+++ music/music.py
@@ -1,45 +1,37 @@
 import streamlit as st
-import base64
-
-# Page title 
+# Page Settings
 
 st.set_page_config(
     page_title="My Music",
     page_icon="🎵",
     layout="wide"
 )
+# Background Image 
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://raw.githubusercontent.com/shubhamverma2026-ai/music-app/e088e33db7fbf9906e15d853e7d80cd09d253e02/background.jpg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    .spotify-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 220px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
-def set_background(image_file):
-    with open(image_file, "rb") as file:
-        encoded = base64.b64encode(file.read()).decode()
-
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/jpeg;base64,{encoded}");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }}
-
-        .spotify-container {{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-top: 220px;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
 
 
-set_background("background.jpg")
-
-# -----------------------------
 playlist_url = "https://open.spotify.com/embed/playlist/4HGxkMbaqCbHetmy0Qd4Xc"
 
 st.markdown(
